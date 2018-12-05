594149351975bca83c541d04486245e950fd42b9
Cowfactory <hhongnr@gmail.com>
2018-12-05 09:23:46 -0800

    heroku deployment config changes

--- server.js
+++ server.js
@@ -19,15 +19,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Mount auth routes with rate limiting middleware
-// app.use('/auth/login', require('./routes/middleware/loginLimiter'));
-// app.use('/auth/signup', require('./routes/middleware/signupLimiter'));
+app.use('/auth/login', require('./routes/middleware/loginLimiter'));
+app.use('/auth/signup', require('./routes/middleware/signupLimiter'));
 app.use('/auth', require('./routes/auth/auth'));
 
 // Mount API routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/bossruns', require('./routes/api/bossruns'));
 app.use('/api/raidgroups', require('./routes/api/raidgroups'));
-// app.use('/raids', require('./routes/api/raids'))
+
+// ToDo: add raids and items features
 // app.use('/items', require('./routes/api/items'))
 
 // This line uses the express-jwt to protect the routes
